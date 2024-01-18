b9265ef0944b6934ac5a9e12bf023cb0a7d5b367
Chirag <panjwanichirag123@gmail.com>
2024-01-19 01:42:46 +0530

    Updating views.py, test.py, settings.py, forms.py

--- first/forms.py
+++ first/forms.py
@@ -1,12 +1,52 @@
 from django import forms
 from .models import Face_Image
+import cv2
+import numpy
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+
+class ProcessedImageField(forms.FileField):
+  def to_python(self, data):
+    raw_content = super().to_python(data)
+    if raw_content is None:
+      return None
+
+    content = raw_content.read()
+    img_array = numpy.frombuffer(content, numpy.uint8)
+    frame = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (48, 48))
+    processed_content = cv2.imencode('.jpg', resized)[1].tobytes()
+
+    # Create an InMemoryUploadedFile instance with processed content
+    processed_file = InMemoryUploadedFile(
+        file=BytesIO(processed_content),
+        field_name=None,
+        name='processed_img.jpg',
+        content_type='image/jpeg',
+        size=len(processed_content),
+        charset=None
+    )
+
+    return processed_file
 
 class ImageForm(forms.ModelForm):
-  class Meta:
-    model = Face_Image
-    fields = ('image',)
-    widgets = {
+    image = ProcessedImageField()
+
+    class Meta:
+        model = Face_Image
+        fields = ('image',)
+        widgets = {
             'image': forms.FileInput(attrs={'accept': 'image/*'}),
         }
-    
-    
+
+    def save(self, commit=True):
+      processed_content = self.cleaned_data.get('image')
+
+      
+      face_image = Face_Image(image=processed_content)
+      if commit:
+        face_image.save()
+
+      return face_image
--- first/views.py
+++ first/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from .forms import ImageForm
 from django.conf import settings
 from models.test import detect_emotion
-import os
+from models.spotify import play_from_playlist
+import cv2,os,numpy
+import urllib.parse
+
+happy_songs_uri = 'spotify:playlist:0TB1scqYXu8vaFwxiOaT1j'
+sad_songs_uri = 'spotify:playlist:6eq5Zviu57AxTT02x0F3pE'
+neutral_songs_uri = 'spotify:playlist:1QbJym8B3Xb6rpXdk7MBiJ'
+
 # Create your views here.
 def home_screen(request):
   # print(request.headers)
@@ -15,13 +22,25 @@ def get_emotion(request):
     if form.is_valid():
       uploaded_img = form.save()
       imgUrl = uploaded_img.image.url
+      print(imgUrl)
       imgPath = imgUrl.lstrip('/')
       emotion = detect_emotion(imgPath)
-      if emotion != '':
-       return render(request,'second.html',{'form':form,'emotion':emotion})
+      if emotion == 'Happy':
+        play_from_playlist(happy_songs_uri)
+        return render(request,'second.html',{'form':form,'emotion':emotion})
+              
+      if emotion == 'Sad':
+        play_from_playlist(sad_songs_uri)
+        return render(request,'second.html',{'form':form,'emotion':emotion})
+              
+      if emotion == 'Neutral':
+        play_from_playlist(neutral_songs_uri) 
+        return render(request,'second.html',{'form':form,'emotion':emotion})
+      
       else:
         form = ImageForm()
         return render(request,'second.html',{'form':form})  
+      
     else:
       form = ImageForm()
       return render(request,'index.html',{'form':form})
